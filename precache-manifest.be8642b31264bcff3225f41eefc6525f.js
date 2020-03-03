95cd1f03647533f53a560ba0ae43342b7a1cea77
coralcat <you@example.com>
2020-03-03 11:30:58 +0900

    Updates

---
+++ precache-manifest.be8642b31264bcff3225f41eefc6525f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59743c34f22fa920f4f4f49663419af6",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "33fab1b040a50ef55eb5",
+    "url": "/react-scoreboard/static/css/main.772dc7f3.chunk.css"
+  },
+  {
+    "revision": "0a5f1c26a5ef0e4cf3c1",
+    "url": "/react-scoreboard/static/js/2.58c446d9.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/react-scoreboard/static/js/2.58c446d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fab1b040a50ef55eb5",
+    "url": "/react-scoreboard/static/js/main.0cc3652f.chunk.js"
+  },
+  {
+    "revision": "a9c64470890cfa2c2789",
+    "url": "/react-scoreboard/static/js/runtime-main.74e123cd.js"
+  }
+]);
